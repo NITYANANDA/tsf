3662bd1e33b2d04e1207f2fe6f798c10442ee077
Colm O hEigeartaigh <coheigea@apache.org>
2011-11-16 16:54:55 +0000

    Extensive re-factor of the jaxws-cxf-sts sample

--- examples/jaxws-cxf-sts/client/src/main/java/client/WSClient.java
+++ examples/jaxws-cxf-sts/client/src/main/java/client/WSClient.java
@@ -7,19 +7,27 @@ import org.example.contract.doubleit.DoubleItPortType;
 import org.example.contract.doubleit.DoubleItService;
 
 public class WSClient {
+    
+    public WSClient() {
+        //
+    }
+    
+    public WSClient(
+        DoubleItPortType portType
+    ) {
+        doubleIt(portType, 10);
+    }
+    
     public static void main (String[] args) {
         DoubleItService service = new DoubleItService();
         DoubleItPortType port = service.getDoubleItPort();           
 
         doubleIt(port, 10);
-        doubleIt(port, 0);
-        doubleIt(port, -10);
     } 
     
     public static void doubleIt(DoubleItPortType port, 
             int numToDouble) {
         int resp = port.doubleIt(numToDouble);
-        System.out.println("The number " + numToDouble + " doubled is " 
-            + resp);
+        System.out.println("The number " + numToDouble + " doubled is " + resp);
     }
 }
